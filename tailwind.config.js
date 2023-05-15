cb8503e549aeeca86f30c2e611867c7fc357590e
권유리 <99jiyusu@gmail.com>
2023-05-16 04:38:48 +0900

    chore: tailwind color 변수 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      'main': '#EC7300',
+      // #ECA459
+      'red': '#DF6B5A',
+      'blue': '#364BB3',
+      'grey': '#EAEAEA',
+      'tab': '#FAFAFA',
+      'sell': '#F3F8FE',
+      'buy': '#FDF3F4',
+      'black': '#000000',
+      'dark-grey': '#606060',
+      'main-light': '#E1EEE8',
+      'white': '#ffffff',
+      'light-grey': '#DADADA'
+    },
+    textColor: {
+      'main': '#EC7300',
+      'white': '#FFFFFF',
+      'black': '#000000',
+      'red': '#DF6B5A',
+      'blue': '#364BB3',
+      'dark-grey': '#606060',
+      'grey': '#434344'
+    },
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 };
